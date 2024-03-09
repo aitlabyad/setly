4bfa9ed752eaf9b138e1f6e5d605a1869ae4d185
AIT LABAYD MOUHCINE <mouhcine.ait.labyad@gmail.com>
2024-03-09 17:39:27 +0100

    suspence home page for clint side rendring

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer";
 import { Poppins } from 'next/font/google';
 import Car from '@/components/car';
 import { cars } from '../../constants/Acars'
-
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
 
 const poppins = Poppins({
@@ -53,46 +53,47 @@ export default function Home() {
   const date2 = searchParams.get('date2')
 
     return (
-
+        <Suspense fallback={<div>Loading...</div>}>
+        {
         <main className="flex  flex-col items-center justify-center ">
 
-            <Header />
-           
-                
+        <Header />
+       
+            
 
-            <div className={`${poppins.className} flex justify-center items-center sm:flex-row sm:justify-between pt-10 pb-4`} >
+        <div className={`${poppins.className} flex justify-center items-center sm:flex-row sm:justify-between pt-10 pb-4`} >
 
-                <div className=" sm:flex-row sm:bg-transparent sm:justify-between sm:w-[1200px] w-[200px] sm:h-[160px] sm:p-0 h-[100px]   rounded-xl flex-col justify-center items-center inline-flex gap-6">
-                    <div className=" w-full  flex-col justify-center items-center gap-2 flex">
-                        <div className=" w-full  text-center text-bold  text-[1.6rem] text-black1 sm:text-[52px] sm:leading-[px]"><span className='font-bold text-black1' >Select </span><span className='font-bold text-gold' >LUXURY </span><span className='font-bold text-black1' >car</span><span> </span></div>
-                        <div className=" w-full text-[22px] text-center text-gray1  font-light  leading-6">RENT NOW</div>
-                    </div>
-
-
-
+            <div className=" sm:flex-row sm:bg-transparent sm:justify-between sm:w-[1200px] w-[200px] sm:h-[160px] sm:p-0 h-[100px]   rounded-xl flex-col justify-center items-center inline-flex gap-6">
+                <div className=" w-full  flex-col justify-center items-center gap-2 flex">
+                    <div className=" w-full  text-center text-bold  text-[1.6rem] text-black1 sm:text-[52px] sm:leading-[px]"><span className='font-bold text-black1' >Select </span><span className='font-bold text-gold' >LUXURY </span><span className='font-bold text-black1' >car</span><span> </span></div>
+                    <div className=" w-full text-[22px] text-center text-gray1  font-light  leading-6">RENT NOW</div>
                 </div>
-            </div>
-
-            {!search ? <div className='sm:w-[1200px] w-11/12  flex text-light text-gold sm:justify-start   justify-center text-bold  items-center gap-2 pb-10 font-light text-[26px] '>
-                Our Fleet
-            </div> : 
-                    <div className='sm:w-[1200px] w-11/12  flex text-light text-gold sm:justify-start   justify-center text-bold  items-center gap-2 pb-10 font-light text-[20px] '>
-                    Availble cars from <span className='text-black1'>{date} </span> to <span className='text-black1'>{date2}</span> : 
-                </div>
-            }
-
-           
-            <div className='grid sm:grid-cols-4  grid-cols-1 gap-4'>
 
 
-
-                {cars.map(function (car) {
-                    return (
-                        <Car key={car.model} car={car} place={search || ''} date1={date || ''} date2={date2|| ''} />
-                    )
-                })}
 
             </div>
+        </div>
+
+        {!search ? <div className='sm:w-[1200px] w-11/12  flex text-light text-gold sm:justify-start   justify-center text-bold  items-center gap-2 pb-10 font-light text-[26px] '>
+            Our Fleet
+        </div> : 
+                <div className='sm:w-[1200px] w-11/12  flex text-light text-gold sm:justify-start   justify-center text-bold  items-center gap-2 pb-10 font-light text-[20px] '>
+                Availble cars from <span className='text-black1'>{date} </span> to <span className='text-black1'>{date2}</span> : 
+            </div>
+        }
+
+       
+        <div className='grid sm:grid-cols-4  grid-cols-1 gap-4'>
+
+
+
+            {cars.map(function (car) {
+                return (
+                    <Car key={car.model} car={car} place={search || ''} date1={date || ''} date2={date2|| ''} />
+                )
+            })}
+
+        </div>
 
 
 
@@ -102,13 +103,16 @@ export default function Home() {
 
 
 
-            <Sectioncontact />
-            <Footer />
+        <Sectioncontact />
+        <Footer />
 
 
 
 
 
-        </main>
+    </main>
+        }
+    </Suspense>
+        
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Suspense } from 'react';
 import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
@@ -26,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} flex justify-center items-center bg-[#F8F8F8]`}>{children}</body>
+      <body className={`${poppins.className} flex justify-center items-center bg-[#F8F8F8]`}>
+        <Suspense>
+          {children}
+        </Suspense>
+      </body>
     </html>
   );
 }
